83692c2089e50597945555c0cd64f0f92c58d25e
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-02-22 22:00:26 -0300

    End of Day 1

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,4 +1,24 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
 
+/**
+ * HTTP Methods
+ *
+ * GET => Busca
+ * POST => Salvar uma informacao
+ * PUT => Alterar uma informacao
+ * DELETE => Deletar
+ * PATCH => Alteracao especifica
+ */
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World - NLW 4" });
+});
+
+// 1 param => Rota (recurso API)
+// 2 param => request, response
+app.post("/", (request, response) => {
+  return response.json({ message: "os dados foram salvos com sucesso" });
+});
+
+app.listen(3333, () => console.log("Server is running at port 3333"));
