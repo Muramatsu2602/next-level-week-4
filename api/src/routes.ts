36f4618886537691bfeea74bdcf2db982a132916
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-02-25 16:56:19 -0300

    Day2: done but still problem in routes

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -3,7 +3,7 @@ import { UserController } from "./controllers/UserController";
 
 const router = Router();
 
-// const UserController = new UserController();
+const UserController = new UserController();
 
 router.post("/users", UserController.create);
 
