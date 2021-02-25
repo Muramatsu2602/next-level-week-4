0585e30a8aa61d3c3b55784dc41e2e7ecd0be396
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-02-25 16:48:45 -0300

    Day2: managing UserController and create User

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -3,6 +3,8 @@ import { UserController } from "./controllers/UserController";
 
 const router = Router();
 
-const UserController = new UserController();
+// const UserController = new UserController();
 
 router.post("/users", UserController.create);
+
+export { router };
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,11 @@
 import "reflect-metadata";
 import express, { response } from "express";
+import { router } from "./routes";
 import "./database"; // already pulls index.ts
 
 const app = express();
+
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running at port 3333"));
